d89f295fbfd94596b47b942988257c34bacb32ee
12345asdfj <140524599+12345asdfj@users.noreply.github.com>
2023-07-31 22:28:11 -0700

    faas work

---
+++ j.js
@@ -0,0 +1,24 @@
+
+// function table() {
+    // document.getElementById('input').innerHTML=''
+// let number =document.getElementById('output'). value
+    // for (let i = 0; i<11; i++)
+    // document.getElementById('input').innerHTML+=
+//    number+" x"+i+ "=" + number*i +"<br>"
+    
+// }
+
+
+
+
+
+function table() {
+document.getElementById('input').innerHTML=''
+    let number= document.getElementById('output').value
+for (let i= 0; i<11; i++)
+document.getElementById('input').innerHTML+=
+ number+" x" +i+ "=" + number*i + "<br>"
+}
+
+
+
